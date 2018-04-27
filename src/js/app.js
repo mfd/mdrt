7c31a5cb41266371d11fd3d15a3fd267fa71d5e6
mfd@home <k@k.ru>
2018-04-27 17:10:22 +0500

    ini
    Former-commit-id: ae924f70623ebd721c97c556dbdff6671609667b

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+import jQuery from 'jquery';
+window.$ = $;
+window.jQuery = jQuery;
+
+import common from './common';
+
+$(document).ready(common);
---
+++ src/js/common.js
@@ -0,0 +1,127 @@
+import $ from 'jquery';
+import jQuery from 'jquery';
+window.$ = $;
+window.jQuery = jQuery;
+
+import 'owl.carousel';
+
+
+export default () => {
+  const BODY = $('body');
+  const overlay = $('.js-overlay');
+  const doc = $(document);
+  const menu = $('.js-menu');
+  //console.log(body[0]);
+
+  //toggle mobile
+  //import {scrollHidden, scrollVisible} from './hiddenShowScrollBar';
+
+  const btn = '.js-toggle-nav';
+  const $btn = $(btn);
+  const nav = '.js-nav';
+  const $nav = $(nav);
+
+  const hideNav = () => {
+    if($nav.hasClass('is-active')) {
+      $btn.removeClass('is-active');
+      $nav.removeClass('is-active');
+      BODY.removeClass('is-hidden');
+      BODY.removeClass('is-nav');
+
+    }
+  };
+
+  $btn.on('click', function() {
+    $(this).toggleClass('is-active');
+    $nav.toggleClass('is-active');
+    BODY.toggleClass('is-hidden');
+    BODY.toggleClass('is-nav');
+    //scrollHidden();
+    if (!($(this).hasClass('is-active'))) {
+      //scrollVisible();
+    }
+  });
+
+  $nav.on('click', function(e) {
+    if($(e.target).hasClass(nav.replace(/\./g,''))) hideNav();
+  });
+
+  $('.sidebar .menu .subMenu').each(function( index ) {
+    // console.log((index*10)+10);
+    // $(this).find('a').css('z-index','999');
+  });
+  $(document).on({
+    mouseenter: function() {
+      $(this).addClass('isOpenMenu');
+      $(this).find('a').css('z-index','999');
+    },
+    mouseleave: function() {
+      $(this).removeClass('isOpenMenu');
+      $(this).find('a').css('z-index','0');
+    }
+  }, '.sidebar .menu .subMenu, .sidebar .smenu .subMenu');
+
+  //   $('.sidebar .menu .subMenu > a, .sidebar .smenu .subMenu > a').each(function( index ) {
+  //     $(this).on('click', function(e) {
+  //       //$('.menu').find('.subMenu').removeClass('isOpenMenu');
+  //       //$('.menu').find('.subMenu a').css('z-index','0');
+  //
+  //       let ifOpen = $(this).parent().hasClass('isOpenMenu');
+  //       console.log(ifOpen);
+  //       if (ifOpen) {
+  //         $(this).parent().removeClass('isOpenMenu');
+  //         $(this).css('z-index','0');
+  //       } else {
+  //         $(this).parent().addClass('isOpenMenu');
+  //         $(this).css('z-index','999');
+  //
+  //       }
+  //       //$(this).parent().toggleClass('isOpenMenu');
+  //       e.preventDefault();
+  //     });
+  //   });
+
+
+
+  let $owl = $('.slider__wrapslides');
+
+  $owl.on('initialized.owl.carousel',function(property) {
+    $('.owl-item.active').find('.slider__title')
+      .addClass('animated flipInX');
+  });
+
+  $owl.owlCarousel({
+    loop: true,
+    responsiveClass:true,
+    nav: true,
+    navigation: true,
+    autoplay:true,
+    items: 1,
+    autoplayTimeout: 6000, //6s
+    // responsive:{
+    //   0:{
+    //     items:1,
+    //     margin: 10,
+    //     dots: true,
+    //   },
+    //   668:{
+    //     margin: 0,
+    //     items:1,
+    //     dots: true
+    //   },
+    //   1480:{
+    //     items:1,
+    //     autoWidth: true,
+    //     dots: true,
+    //     margin: 40
+    //   }
+    // }
+  });
+
+  $('.custom-slick-next').click(function(event) {
+    $owl.trigger('next.owl.carousel');
+  });
+
+
+
+};
